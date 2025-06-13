3d68eec786eef92993b7756777faa8ec4f5e03cc
Beth K <bethknights61@gmail.com>
2025-06-13 12:34:27 +0100

    Added file

---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    print("Hello, world!")
+
+if __name__ == "__main__":
+    main()
+# This is a simple Python script that prints "Hello, world!" to the console.
+# It serves as a basic example of how to structure a Python program.
+# To run this script, save it as test.py and execute it using the command:
+# python test.py
